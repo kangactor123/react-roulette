c6495173363610cac679e9ec4e59b8e0ac7f1276
강동희 <kangactor123@naver.com>
2023-12-28 13:19:06 +0900

    방장사기 제거, 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import "./app.css";
+import styled from "styled-components";
 
 function App() {
   const [value, setValue] = useState("");
@@ -25,11 +25,11 @@ function App() {
           ctx.canvas.style.transition = `2s`;
 
           setTimeout(() => {
-            const regExp = new RegExp("^조");
-            const winner = list.find(
-              (li) => regExp.test(li) || li.includes("영빈")
-            );
-            alert(`당첨자는 바로! ${winner || list[ran]} 입니다!`);
+            // const regExp = new RegExp("^조");
+            // const winner = list.find(
+            //   (li) => regExp.test(li) || li.includes("영빈")
+            // );
+            alert(`당첨자는 바로! ${list[ran]} 입니다!`);
           }, 2000);
         }, 1);
       }
@@ -92,53 +92,130 @@ function App() {
   }, [colorList, list]);
 
   return (
-    <div>
-      <div>
+    <Wrapper>
+      <div className="input-wrap">
         <div>참여자를 한명씩 입력해주세요.</div>
-        <input value={value} onChange={(e) => setValue(e.target.value)} />
-        <button
-          onClick={() => {
-            setList((prev) => [...prev, value]);
-            setColorList((prev) => {
-              const color = `rgb(${Math.floor(
-                Math.random() * 256
-              )},${Math.floor(Math.random() * 256)},${Math.floor(
-                Math.random() * 256
-              )})`;
-              return [...prev, color];
-            });
-            setValue("");
-          }}
-        >
-          추가하기
-        </button>
+        <div>
+          <Input value={value} onChange={(e) => setValue(e.target.value)} />
+          <AddButton
+            onClick={() => {
+              if (value === "") return;
+              setList((prev) => [...prev, value]);
+              setColorList((prev) => {
+                const color = `rgb(${Math.floor(
+                  Math.random() * 256
+                )},${Math.floor(Math.random() * 256)},${Math.floor(
+                  Math.random() * 256
+                )})`;
+                return [...prev, color];
+              });
+              setValue("");
+            }}
+          >
+            추가하기
+          </AddButton>
+        </div>
       </div>
       <div>
-        참여자 리스트:
-        {list.map((li, idx) => (
+        {list.length === 0 ? (
+          "참가자가 없습니다!"
+        ) : (
           <>
-            <span>{li}</span>
-            {idx !== list.length - 1 && ", "}
+            참가자 리스트:
+            {list.map((li, idx) => (
+              <>
+                <span>{li}</span>
+                {idx !== list.length - 1 && ", "}
+              </>
+            ))}
           </>
-        ))}
+        )}
       </div>
       {list.length === 0 ? (
         <div>참가자를 입력해주세요</div>
       ) : (
         <div className="canvas-wrap">
-          <canvas ref={ref} width="600" height="600" />
+          <canvas ref={ref} width={300} height={300} />
         </div>
       )}
-      <button
-        onClick={() => {
-          if (list.length === 0) alert("참여자가 없습니다!!");
-          else rotateRoullete();
-        }}
-      >
-        룰렛 돌리기!!
-      </button>
-    </div>
+      <div>
+        {list.length ? (
+          <RotateButton
+            onClick={() => {
+              if (list.length === 0) alert("참여자가 없습니다!!");
+              else rotateRoullete();
+            }}
+          >
+            룰렛 돌리기!!
+          </RotateButton>
+        ) : null}
+      </div>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Input = styled.input`
+  width: 180px;
+  height: 30px;
+  padding: 6px 12px;
+  border: none;
+  border-bottom: 1px solid gray;
+`;
+
+const AddButton = styled.button`
+  width: 90px;
+  height: 36px;
+  border-radius: 12px;
+  background-color: #e6b9de;
+  color: #4942e4;
+  font-weight: 600;
+  border: none;
+  cursor: pointer;
+`;
+
+const RotateButton = styled.button`
+  width: 180px;
+  height: 50px;
+  border-radius: 15px;
+  border: 1px dashed beige;
+  cursor: pointer;
+  background-color: #dc8686;
+
+  &:hover {
+    background-color: blue;
+    color: #fff;
+  }
+`;
+
+const Wrapper = styled.div`
+  max-width: 768px;
+  min-width: 320px;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding-top: 24px;
+  background-color: #fff;
+
+  & .input-wrap {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    & div:nth-child(2) {
+      display: flex;
+      gap: 6px;
+      align-items: flex-end;
+    }
+  }
+
+  & .canvas-wrap {
+    width: 300px;
+    height: 300px;
+    margin: 60px;
+  }
+`;
---
+++ src/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset}
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    input, textarea { 
+      -moz-user-select: auto;
+      -webkit-user-select: auto;
+      -ms-user-select: auto;
+      user-select: auto;
+    }
+    input:focus {
+      outline: none;
+    }
+
+    button {
+      border: none;
+      background: none;
+      padding: 0;
+      cursor: pointer;
+    }
+
+    html, body {
+      width: 100%;
+      height:100%;
+    }
+
+    #root {
+        width: 100%;
+        height: 100%;
+        background-color: #EEF5FF;
+    }
+`;
+
+export default GlobalStyles;
